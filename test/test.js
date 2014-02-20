de199dd9ecb8f36183284397538c613601382bd5
ankostyuk <andrew.kostyuk@gmail.com>
2014-02-20 21:46:42 +0400

    Тесты

--- test/test.js
+++ test/test.js
@@ -2,13 +2,49 @@
  * @author ankostyuk
  */
 
-var chai        = require('chai'),
+var fs          = require('fs-extra'),
+    chai        = require('chai'),
     assert      = chai.assert,
     expect      = chai.expect,
     should      = chai.should();
 
-describe('', function(){
-    it('', function(done){
-        done();
+var wb = require('../src/');
+
+describe('web-bin test...', function(){
+
+    it('Процессинг ресурсов', function(done){
+
+        var options = {
+            skipProcess: false,
+            inputDir: __dirname + '/process-resources/input/src',
+            outputDir: __dirname + '/process-resources/output/src'
+        };
+
+        fs.removeSync(options.outputDir);
+        fs.mkdirsSync(options.outputDir);
+
+        wb.processResources.run(options, function(){
+            done();
+        });
+    })
+
+    it('Оптимизация requirejs', function(done){
+
+        var options = {
+            skipOptimize: true,
+            propertiesFile: __dirname + '/requirejs-optimize/web-resources-build.properties',
+            mainFile: '',
+            requirejs: {
+                dir: __dirname + '/requirejs-optimize/build'
+            }
+        };
+
+
+        fs.removeSync(options.requirejs.dir);
+        fs.mkdirsSync(options.requirejs.dir);
+
+        wb.requirejsOptimize.run(options, function(){
+            done();
+        });
     })
 })
